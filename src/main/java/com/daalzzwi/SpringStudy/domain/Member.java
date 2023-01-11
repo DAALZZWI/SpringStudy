47a797d128cf86d9f1a83383f0b3f7df83244c29
daalzzwi <daalzzwi@gmail.com>
2023-01-11 13:14:16 +0900

    MemberRepository 추가 완료

---
+++ src/main/java/com/daalzzwi/SpringStudy/domain/Member.java
@@ -0,0 +1,23 @@
+package com.daalzzwi.SpringStudy.domain;
+
+public class Member {
+
+    private Long id;
+    private String getId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGetId() {
+        return getId;
+    }
+
+    public void setGetId(String getId) {
+        this.getId = getId;
+    }
+}
